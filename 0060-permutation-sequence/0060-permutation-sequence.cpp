fedad47af0541dffdfe2416309681252c8add8ad
huy2451220088-lgtm <huy_2451220088@dau.edu.vn>
2026-05-26 14:23:41 +0700

    Time: 0 ms (100%), Space: 8.7 MB (74.31%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> numbers;
+        int fact = 1;
+        for (int i = 1; i < n; i++) {
+            fact *= i;
+            numbers.push_back(i);
+        }
+        numbers.push_back(n);
+        
+        string res = "";
+        k = k - 1;
+        while (true) {
+            res += to_string(numbers[k / fact]);
+            numbers.erase(numbers.begin() + k / fact);
+            if (numbers.empty()) break;
+            k %= fact;
+            fact /= numbers.size();
+        }
+        return res;
+    }
+};

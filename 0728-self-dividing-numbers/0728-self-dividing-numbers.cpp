f60b4e993d34bb977a4917f544265a062ad8020a
huy2451220088-lgtm <huy_2451220088@dau.edu.vn>
2026-05-26 13:40:53 +0700

    Time: 0 ms (100%), Space: 9.3 MB (25.62%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> result;
+        
+        for (int i = left; i <= right; i++) {
+            if (isSelfDividing(i)) {
+                result.push_back(i);
+            }
+        }
+        
+        return result;
+    }
+
+private:
+    bool isSelfDividing(int num) {
+        int temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            // Số tự phân chia không được chứa chữ số 0 
+            // và số đó phải chia hết cho từng chữ số của nó
+            if (digit == 0 || num % digit != 0) {
+                return false;
+            }
+            temp /= 10;
+        }
+        return true;
+    }
+};

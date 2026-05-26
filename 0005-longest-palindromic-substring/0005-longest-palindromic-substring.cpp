e657223276f98966c00214393ff05cf30e6a5e5b
huy2451220088-lgtm <huy_2451220088@dau.edu.vn>
2026-05-26 13:57:28 +0700

    Time: 7 ms (87.83%), Space: 9.5 MB (66.7%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        int start = 0, max_len = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // Trường hợp chuỗi đối xứng lẻ (ví dụ: "aba")
+            int len1 = expandAroundCenter(s, i, i);
+            // Trường hợp chuỗi đối xứng chẵn (ví dụ: "bb")
+            int len2 = expandAroundCenter(s, i, i + 1);
+            
+            int len = max(len1, len2);
+            if (len > max_len) {
+                max_len = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+        
+        return s.substr(start, max_len);
+    }
+
+private:
+    int expandAroundCenter(const string& s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+};
